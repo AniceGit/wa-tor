55ce8959c7841cb153b54da683e32d5ffe8a3228
Guiren Anice <anice.guiren@gmail.com>
2025-05-07 15:35:44 +0200

    update de la classe poisson

--- src/models/poisson.py
+++ src/models/poisson.py
@@ -1,4 +1,3 @@
-import random
 from src.models.grille import Grille
 
 class Poisson:
@@ -7,41 +6,13 @@ class Poisson:
         self.abscisse = abscisse
         self.ordonnee = ordonnee
 
-    # def move(self, sea:Sea):
-    #     # dx = random.choice([-1, 0, 1])
-    #     # dy = random.choice([-1, 0, 1])
-    #     dx = random.randint(-1, 1)
-    #     dy = random.randint(-1, 1)
+    def deplacer(self, x, y):
+        self.abscisse = x
+        self.ordonnee = y
+    
 
-    #     new_x = self.abscisse + dx
-    #     new_y = self.ordonnee + dy
-
-    #     if 0 <= new_x < len(sea.sea[0]) and 0 <= new_y < len(sea.sea):
-    #         if sea.sea[new_x][new_y] is None:
-    #             sea.sea[self.abscisse][self.ordonnee] = None
-
-    #             self.abscisse = new_x
-    #             self.ordonnee = new_y
-
-    #             sea.sea[new_x][new_y] = self
-
-    def deplacer(self, width, length, isNone):
-        # dx = random.choice([-1, 0, 1])
-        # dy = random.choice([-1, 0, 1])
-        dx = random.randint(-1, 1)
-        dy = random.randint(-1, 1)
-
-        new_x = self.abscisse + dx
-        new_y = self.ordonnee + dy
-
-        if 0 <= new_x < length and 0 <= new_y < width:
-            if isNone:
-                return
-
-        
-    # def reproduire(self, sea:Sea):
-    #     age = self.age
-    #     tps_gestation = self.tps_gestation
-
-    #     if age%tps_gestation == 0 :
-    #         sea.add_fish()
+    def reproduire(self):
+        if self.tps_gestation <= 0:
+            self.tps_gestation = 5
+            return self 
+        return None   
